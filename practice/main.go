691a4c0b2f43f2a724ad57d9ab7af8b7a5c7ee96
aditya <aa1633350@gmail.com>
2023-09-05 17:55:18 +0530

    Reading input from console.

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+
+	reader := bufio.NewReader(os.Stdin) // bufio package is used for I/O operations.
+	fmt.Print("Enter your text: ")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("You entered string", input)
 }
